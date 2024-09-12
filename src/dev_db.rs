532989ea3765f2233a06843a69808b9cd12379c9
Sam Johnson <sam@durosoft.com>
2024-09-12 00:21:29 -0400

    attach to clickhouse server process

--- src/dev_db.rs
+++ src/dev_db.rs
@@ -1,3 +1,34 @@
+use std::process::{exit, Command, Stdio};
+
 pub fn spawn_click_house() {
     tracing::info!("spawning local ClickHouse server...");
+    // Build the command to run the ClickHouse binary
+    let clickhouse_command = Command::new("clickhouse")
+        .arg("server")
+        .current_dir("./bin")
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn();
+
+    // Check if the process started successfully
+    match clickhouse_command {
+        Ok(mut child) => {
+            tracing::info!("ClickHouse started successfully with PID: {}", child.id());
+
+            // Wait for the process to exit
+            match child.wait() {
+                Ok(status) => {
+                    tracing::info!("ClickHouse exited with status: {}", status);
+                }
+                Err(err) => {
+                    tracing::error!("Failed to wait on the ClickHouse process: {}", err);
+                    exit(1)
+                }
+            }
+        }
+        Err(err) => {
+            tracing::error!("Failed to start the ClickHouse process: {}", err);
+            exit(1)
+        }
+    }
 }
