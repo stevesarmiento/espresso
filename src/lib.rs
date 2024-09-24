5f16247e9eb1280374ba1eefff205a5ca5c30c65
Sam Johnson <sam@durosoft.com>
2024-09-24 17:17:11 -0400

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,27 @@
-use solana_geyser_plugin_interface::geyser_plugin_interface::{GeyserPlugin, Result};
+use std::error::Error;
 
-pub mod dev_db;
+use solana_geyser_plugin_interface::geyser_plugin_interface::{
+    GeyserPlugin, GeyserPluginError, Result,
+};
+use tokio::runtime::Runtime;
+
+pub mod db;
 
 #[derive(Clone, Debug, Default)]
 pub struct Solira;
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+pub enum SoliraError {
+    ClickHouseInitializationFailed,
+}
+
+impl Error for SoliraError {}
+
+impl std::fmt::Display for SoliraError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "ClickHouse initialization failed")
+    }
+}
 
 impl GeyserPlugin for Solira {
     fn name(&self) -> &'static str {
@@ -12,6 +30,25 @@ impl GeyserPlugin for Solira {
 
     fn on_load(&mut self, _config_file: &str, _is_reload: bool) -> Result<()> {
         solana_logger::setup_with_default("info");
+
+        let rt = Runtime::new()?;
+
+        // Spawn a background task without blocking
+        rt.block_on(async {
+            // Spawn the ClickHouse process and await the readiness signal
+            let (mut ready_rx, clickhouse_future) = db::spawn_click_house().await.unwrap();
+
+            // Wait until ClickHouse is ready to accept connections
+            if ready_rx.recv().await.is_some() {
+                log::info!("ClickHouse initialization complete.");
+
+                // Optionally, spawn the ClickHouse process in the background
+                tokio::spawn(clickhouse_future);
+            } else {
+                // return Err(GeyserPluginError::Custom(Box::new(SoliraError::ClickHouseInitializationFailed)));
+            }
+        });
+
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,7 @@
-use std::{env, process::exit};
-
-pub mod dev_db;
+pub mod db;
 
 #[tokio::main]
 async fn main() {
-    solana_logger::setup_with_default("info");
-    let _ = dotenvy::from_path(".env");
-
-    if env::var("DEV_MODE").is_ok() {
-        log::info!("dev mode is enabled");
-        // Spawn the ClickHouse process and await the readiness signal
-        let (mut ready_rx, clickhouse_future) = dev_db::spawn_click_house().await.unwrap();
-
-        // Wait until ClickHouse is ready to accept connections
-        if ready_rx.recv().await.is_some() {
-            log::info!("ClickHouse dev-mode initialization complete.");
-
-            // Optionally, spawn the ClickHouse process in the background
-            tokio::spawn(clickhouse_future);
-        } else {
-            log::error!("Failed to receive ClickHouse readiness signal.");
-            exit(1)
-        }
-    }
-
-    // "hello world" is logged at the end of the `main` function, regardless of DEV_MODE
     log::info!("solira initialized");
     loop {
         tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
