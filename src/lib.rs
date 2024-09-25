2dff13a7fff1e4ccc9ad149aa6331a934ff6d6c4
Sam Johnson <sam@durosoft.com>
2024-09-25 13:00:21 -0400

    on_load hook working

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,21 @@
-use std::error::Error;
-
 use solana_geyser_plugin_interface::geyser_plugin_interface::{
     GeyserPlugin, GeyserPluginError, Result,
 };
+use std::{cell::RefCell, error::Error};
 use tokio::runtime::Runtime;
 
 pub mod db;
+
+thread_local! {
+    static TOKIO_RUNTIME: RefCell<Runtime> = RefCell::new(Runtime::new().unwrap());
+}
 
 #[derive(Clone, Debug, Default)]
 pub struct Solira;
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub enum SoliraError {
+    ClickHouseThreadSpawnFailed,
     ClickHouseInitializationFailed,
 }
 
@@ -19,7 +23,14 @@ impl Error for SoliraError {}
 
 impl std::fmt::Display for SoliraError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "ClickHouse initialization failed")
+        match self {
+            SoliraError::ClickHouseThreadSpawnFailed => {
+                write!(f, "Failed to spawn ClickHouse thread")
+            }
+            SoliraError::ClickHouseInitializationFailed => {
+                write!(f, "ClickHouse initialization failed")
+            }
+        }
     }
 }
 
@@ -31,24 +42,28 @@ impl GeyserPlugin for Solira {
     fn on_load(&mut self, _config_file: &str, _is_reload: bool) -> Result<()> {
         solana_logger::setup_with_default("info");
 
-        let rt = Runtime::new()?;
+        // Use a closure with a return type `Result<()>`
+        TOKIO_RUNTIME.with(|rt_cell| {
+            let rt = rt_cell.borrow();
+            rt.block_on(async {
+                let (mut ready_rx, clickhouse_future) =
+                    db::spawn_click_house().await.map_err(|e| {
+                        log::error!("error: {}", e);
+                        GeyserPluginError::Custom(Box::new(
+                            SoliraError::ClickHouseThreadSpawnFailed,
+                        ))
+                    })?;
 
-        // Spawn a background task without blocking
-        rt.block_on(async {
-            // Spawn the ClickHouse process and await the readiness signal
-            let (mut ready_rx, clickhouse_future) = db::spawn_click_house().await.unwrap();
-
-            // Wait until ClickHouse is ready to accept connections
-            if ready_rx.recv().await.is_some() {
-                log::info!("ClickHouse initialization complete.");
-
-                // Optionally, spawn the ClickHouse process in the background
-                tokio::spawn(clickhouse_future);
-            } else {
-                // return Err(GeyserPluginError::Custom(Box::new(SoliraError::ClickHouseInitializationFailed)));
-            }
-        });
-
-        Ok(())
+                if ready_rx.recv().await.is_some() {
+                    log::info!("ClickHouse initialization complete.");
+                    rt.spawn(clickhouse_future);
+                    Ok(())
+                } else {
+                    Err(GeyserPluginError::Custom(Box::new(
+                        SoliraError::ClickHouseInitializationFailed,
+                    )))
+                }
+            })
+        })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use std::time::Duration;
+
+use solana_geyser_plugin_interface::geyser_plugin_interface::GeyserPlugin;
+use solira::*;
+
 pub mod db;
 
-#[tokio::main]
-async fn main() {
-    log::info!("solira initialized");
+fn main() {
+    log::info!("solira launched in local dev mode");
+    let mut solira = Solira::default();
+    solira.on_load("", false).unwrap();
+    log::info!("on_load complete");
     loop {
-        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        std::thread::sleep(Duration::from_secs(1));
     }
 }
