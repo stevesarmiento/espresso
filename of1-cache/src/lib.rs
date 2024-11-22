d40e635dfe0135a626ee4d9377f89083da427898
Sam Johnson <sam@durosoft.com>
2024-11-22 18:00:14 -0500

    mmap solution WIP

--- of1-cache/src/lib.rs
+++ of1-cache/src/lib.rs
@@ -1,7 +1,18 @@
-use std::ops::Range;
+use std::{cell::RefCell, fs::File};
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+use memmap2::Mmap;
+
+#[derive(Debug)]
 pub struct Epoch {
     pub num: u32,
-    pub slots: Range<u64>,
+    pub slot_start: u64,
+    pub slot_end: u64,
+    data: RefCell<CarData>,
+}
+
+#[derive(Debug)]
+pub struct CarData {
+    size: usize,
+    file: File,
+    mmap: Mmap,
 }
