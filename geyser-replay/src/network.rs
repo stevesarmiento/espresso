24b1a06e23f2495fdfdb384d5b8b8feb7359dbc2
Sam Johnson <sam@durosoft.com>
2025-03-18 18:37:57 -0400

    WIP

--- geyser-replay/src/network.rs
+++ geyser-replay/src/network.rs
@@ -3,7 +3,7 @@ use std::{fmt::Display, ops::Range};
 use reqwest::Client;
 use thiserror::Error;
 
-use crate::slot_cache::SlotCache;
+use crate::slot_cache::{fetch_epoch_stream_async, SlotCache};
 
 #[derive(Debug, Error)]
 pub enum GeyserReplayError {
@@ -18,12 +18,25 @@ impl Display for GeyserReplayError {
     }
 }
 
+impl From<reqwest::Error> for GeyserReplayError {
+    fn from(e: reqwest::Error) -> Self {
+        GeyserReplayError::Reqwest(e)
+    }
+}
+
 pub async fn process_slot_range(
     slot_range: Range<u64>,
+    epoch_range: Range<u64>,
     client: Client,
-    cache: SlotCache,
 ) -> Result<(), GeyserReplayError> {
-    todo!()
+    for (epoch_num, stream) in epoch_range
+        .map(|epoch| fetch_epoch_stream_async(epoch, &client))
+        .enumerate()
+    {
+        tracing::info!("Processing epoch {}", epoch_num);
+        let stream = stream.await?;
+    }
+    Ok(())
 }
 
 pub struct MessageAddressLoaderFromTxMeta {
@@ -53,4 +66,13 @@ impl Clone for MessageAddressLoaderFromTxMeta {
             tx_meta: self.tx_meta.clone(),
         }
     }
+}
+
+#[test]
+fn test_process_slot_range() {
+    let client = reqwest::Client::new();
+    let slot_range = 700..705;
+    let epoch_range = 700..705;
+    let result = process_slot_range(slot_range, epoch_range, client);
+    assert!(result.is_ok());
 }
