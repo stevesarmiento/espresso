870d0d6f68c14216179c6e649e429b5e10c9254c
Sam Johnson <sam@durosoft.com>
2025-05-07 23:27:19 -0400

    basic interface

--- src/plugin.rs
+++ src/plugin.rs
@@ -2,15 +2,36 @@ pub use agave_geyser_plugin_interface::geyser_plugin_interface::GeyserPluginErro
 pub use agave_geyser_plugin_interface::geyser_plugin_interface::ReplicaBlockInfoVersions as Block;
 pub use agave_geyser_plugin_interface::geyser_plugin_interface::ReplicaTransactionInfoVersions as Transaction;
 
-pub struct PluginState {}
+use ::clickhouse::Client;
 
 pub trait Plugin {
+    /// If true, this plugin must process transactions in order sequentially
+    const ORDER_DEPENDENT: bool = false;
+
+    /// The name of this [`Plugin`]
+    ///
+    /// Used for logging and debugging purposes.
     fn name() -> &'static str;
+
+    /// Called by Solira whenever a transaction is received from the firehose.
+    ///
+    /// `db` contains a reference to the ClickHouse database client.
     fn on_transaction(
         &mut self,
+        db: &Client,
         transaction: Transaction,
         slot: u64,
         epoch: u16,
     ) -> Result<(), GeyserPluginError>;
-    fn on_block(&mut self, block: Block, slot: u64, epoch: u16) -> Result<(), GeyserPluginError>;
+
+    /// Called by Solira whenever a new block is received from the firehose.
+    ///
+    /// This will precede the `on_transaction` call for each transaction in the block.
+    fn on_block(
+        &mut self,
+        db: &Client,
+        block: Block,
+        slot: u64,
+        epoch: u16,
+    ) -> Result<(), GeyserPluginError>;
 }
