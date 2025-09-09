e9ba109f5edddacde1164910ff2e5eb35db04c95
Sam Johnson <sam@durosoft.com>
2025-09-09 00:39:20 -0400

    fix?

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use jetstreamer::JetstreamerRunner;
 use jetstreamer_plugin::plugins::program_tracking::ProgramTrackingPlugin;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(JetstreamerRunner::default()
         .with_log_level("info")
         .parse_cli_args()?
